26c966ee30478dd8e1ad40d222ddd09974dcf9a7
markramach <mark.ramach@centurylink.com>
2018-12-17 11:17:42 -0800

    major version

--- bootsy-core/src/main/java/com/flyover/bootsy/core/Version.java
+++ bootsy-core/src/main/java/com/flyover/bootsy/core/Version.java
@@ -9,8 +9,8 @@ package com.flyover.bootsy.core;
  */
 public class Version {
 	
-	public static final String KUBE_VERSION = "1.10.5";
-	public static final String DOCKER_REPO = "portr.ctnr.ctl.io/bootsy";
+	public static final String KUBE_VERSION = "v1.10.11";
+	public static final String DOCKER_REPO = "portr.ctl.io/bootsy";
 	
 	public static String image(String name) {
 		return String.format("%s/%s:%s", DOCKER_REPO, name, KUBE_VERSION);
